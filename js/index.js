182c8e3d0d97bc72931f0fd61ec7579e114bd794
elyorbekanvarov <e0275king@gmail.com>
2024-05-21 15:07:39 +0500

    3 - dars

---
+++ js/index.js
@@ -0,0 +1,130 @@
+// 1 - masala
+
+// let a = +prompt('sonini kiriting');
+
+// if (a > 0) {
+//   console.log("A soni musbat"); 
+// } else {
+//   console.log("A soni musbat emas");
+// }
+
+// 2 - masala
+
+// function isOdd(a) {
+//   if (a % 2 !== 0) {
+//     return true; 
+//   } else {
+//     return false;
+//   }
+// }
+// let a = 7;
+// if (isOdd(a)) {
+//   console.log("A soni toq son"); 
+// } else {
+//   console.log("A soni toq son emas");
+// }
+
+// 3 - masala
+
+// let a = +prompt('sonini kiriting');
+
+// if (a/2) {
+//   console.log("A soni musbat"); 
+// } else {
+//   console.log("A soni musbat emas");
+// }
+
+// 4 - masala
+
+// let (a) = +prompt('A')
+// let (a) = +prompt('B')
+// if (A>2 && B<=3) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 5 - masala
+
+// let (son1) = +prompt('A sonni kiriting')
+// let (son2) = +prompt('B sonini kiriting')
+// if (son1>=0||son2<-2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 6 - masala
+
+// let A =+prompt("A sonini kiriting")
+// let B =+prompt("B sonini kiriting")
+// let C =+prompt("C sonini kiriting")
+// if (A<=B<-2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 7 - masala 
+
+// let a = 5;
+// let b = 7;
+// let c = 10;
+// if ((a < b && b < c) || (c < b && b < a)) {
+//   console.log("B soni A va C sonlari orasida yotadi");
+// } else {
+//   console.log("B soni A va C sonlari orasida yotmaydi");
+// }
+
+// 8 - masala
+
+// let A =+prompt('A sonini yozing')
+// let B =+prompt("B sonini yozing")
+// if (A % 2 !== 0) {
+//     console.log(true);
+// } else (B % 2 !== 0) {
+//     console.log(false);
+// }
+
+// 9 - masala
+
+// let a = 4;
+// let b = 7;
+
+// if ((a % 2 !== 0) || (b % 2 !== 0)) {
+//   console.log("toq son"); 
+// } else {
+//   console.log("toq son emas"); 
+// }
+
+// 10 - masala
+
+// let a = 5;
+// let b = 8;
+// if ((a % 2 !== 0 && b % 2 === 0) || (a % 2 === 0 && b % 2 !== 0)) {
+//   console.log("toq son"); 
+// } else {
+//   console.log("toq son emas"); 
+// }
+
+// 11 - masala
+
+// let a = 4;
+// let b = 8;
+// if ((a % 2 === 0 && b % 2 === 0) || (a % 2 !== 0 && b % 2 !== 0)) {
+//   console.log("toq yoki juft son"); 
+// } else {
+//   console.log("toq yoki juft son emas"); 
+// }
+
+// 12 - masala
+
+// let a = 5;
+// let b = 10;
+// let c = 2;
+
+// if (a > 0 && b > 0 && c > 0) {
+//   console.log("har biri musbat");
+// } else {
+//   console.log("har biri musbat emas");
+// }
